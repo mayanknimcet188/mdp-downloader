1e691d3eb29d0785e08f5ba4bedc9f6e69226bfa
mayanknimcet188 <205119053@nitt.edu>
2021-01-20 13:29:35 +0530

    Added feature for links from a csv file

--- mdp-downloader.py
+++ mdp-downloader.py
@@ -1,7 +1,14 @@
 from __future__ import unicode_literals
 import youtube_dl
+import csv
+
+urls = []
+with open('./links.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        urls.extend(row)
 
 ydl_opts = {}
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://bae.sgp1.digitaloceanspaces.com/videos/142/dash/3c328863dbadd9c5ca8f7bc444a6ea73.mpd'])
+    ydl.download(urls)
